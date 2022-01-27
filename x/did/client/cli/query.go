b5b7321366ccc18e4d9ee773a4994fc7e3326c6a
arnabghose997 <arnab.ghose128@gmail.com>
2022-01-27 19:42:24 +0530

    Implemented  REST endpoint to get the list of schemas, and created a new store that keeps the complete Schema structure as value

--- x/did/client/cli/query.go
+++ x/did/client/cli/query.go
@@ -33,6 +33,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 
 	cmd.AddCommand(CmdSchemaCount())
 
+	cmd.AddCommand(CmdSchemas())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
