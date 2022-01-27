10f339d87d6e20debaafded457f2af97c1809ac7
arnabghose997 <arnab.ghose128@gmail.com>
2022-01-27 14:35:14 +0530

    Changed DID Module GET endpoint from /hypersign-protocol/hidnode/did to /hypersign-protocol/hidnode/ssi

--- x/did/types/query.pb.gw.go
+++ x/did/types/query.pb.gw.go
@@ -339,11 +339,11 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"hypersignprotocol", "hidnode", "did", "params"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"hypersign-protocol", "hidnode", "ssi", "params"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_Dids_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 2}, []string{"hypersign-protocol", "hidnode", "did"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_Dids_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 3}, []string{"hypersign-protocol", "hidnode", "ssi", "did"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_GetSchema_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"hypersign-protocol", "hidnode", "did", "schema", "schemaId"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_GetSchema_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"hypersign-protocol", "hidnode", "ssi", "schema", "schemaId"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
