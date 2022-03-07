47470e6604e60f55e071a5684b5a1e639c714de8
Vishwas <vishwasanand.cse@gmail.com>
2022-03-07 23:39:43 +0530

    fixed the wallet -  only thing left is , we need to set hid prefix somewhere , I am not sure where

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -2,7 +2,7 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  transpileDependencies: true,
+  transpileDependencies: ['@starport/client-js'],
   configureWebpack: {
     resolve: {
       symlinks: false,
