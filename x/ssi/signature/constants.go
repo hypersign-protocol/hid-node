56b2353eba7b535da357c746377135b38351ef56
arnabghose997 <arnab.ghose128@gmail.com>
2022-12-02 20:22:29 +0530

    typo fix

--- x/ssi/signature/constants.go
+++ x/ssi/signature/constants.go
@@ -1,9 +1,9 @@
 package signature
 
-// Support Verification Method Types
-
+// Supported Verification Method Types
 const Ed25519VerificationKey2020 = "Ed25519VerificationKey2020"
 
+// Mapping between Verification Key and its corresponding Signature
 var verificationKeySignatureMap = map[string]string{
 	"Ed25519VerificationKey2020": "Ed25519Signature2020",
 }
